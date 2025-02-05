8c4a16e11e078e5fa8574546008af8e49cb3a7bc
barkinunal <barkinunal@gmail.com>
2025-02-05 02:08:57 +0000

    colorscheme,  <leader>o

--- lua/barkin/lazy/colorscheme.lua
+++ lua/barkin/lazy/colorscheme.lua
@@ -29,3 +29,19 @@ return {
 		vim.cmd("colorscheme kanagawa")
 	end,
 }
+
+-- return {
+-- 	-- 	"catppuccin/nvim",
+-- 	-- 	name = "catppuccin",
+-- 	-- 	priority = 1000,
+-- 	-- 	config = function()
+-- 	-- 		vim.cmd.colorscheme("catppuccin")
+-- 	-- 	end,
+-- }
+-- return {
+-- 	-- "rose-pine/neovim",
+-- 	-- name = "rose-pine",
+-- 	-- config = function()
+-- 	-- 	vim.cmd("colorscheme rose-pine")
+-- 	-- end,
+-- }
