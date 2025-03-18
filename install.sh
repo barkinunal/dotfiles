4a5ccb87dfafca658a752f1ddd02452ed8288e95
Barkin Unal <barkinunal@gmail.com>
2025-03-18 16:22:16 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@ echo "Install zsh"
 apt-get install zsh
 
 echo "Install nvim"
-apt-get install nvim
+apt-get install neovim
+
+echo "Install ripgrep"
+apt-get install ripgrep
 
 echo "Move nvim config file to .config"
-cp -r nvim/ ~/.config/
+cp -r ./nvim/ ~/.config/
 
 echo "Move zshrc to the right place"
-cp zsh/.zshrc ~/
+cp ./zsh/.zshrc ~/
 
 
